20576942f4b2dccf81def6741338ec4b8d2f97c6
Yeimer <mendezyeimer@gmail.com>
2023-11-21 14:17:22 -0500

    Agrega Campo de texto y respectivo su respectivo label

--- src/Vista/AddContactoPanel.java
+++ src/Vista/AddContactoPanel.java
@@ -30,7 +30,7 @@ this.tipoContacto = tipoContacto;
         panelContenido = new JPanel(new BorderLayout());
 
 
-        // Panel Botones
+        // Panel Datos
 
         JPanel panelDatos = new JPanel(new GridBagLayout());
 
@@ -50,21 +50,56 @@ this.tipoContacto = tipoContacto;
         txtfId = new JTextField("",10);
         panelDatos.add(txtfId);
 
+        gbc.gridx = 2;
+        lblDirecciones = new JLabel("Direccion");
+        panelDatos.add(lblDirecciones,gbc);
+
+        gbc.gridx = 3;
+        txtfDirecciones = new JTextField("",10);
+        panelDatos.add(txtfDirecciones,gbc);
+
         gbc.gridx = 0;
         gbc.gridy = 1;
         lblFechaN = new JLabel("Fecha de Nacimiento");
         panelDatos.add(lblFechaN,gbc);
 
+        gbc.gridx = 1;
+        txtfFechaN = new JTextField("",10);
+        panelDatos.add(txtfFechaN,gbc);
+
+        gbc.gridx = 2;
+        lblTelefonos = new JLabel("Telefono");
+        panelDatos.add(lblTelefonos,gbc);
+
+        gbc.gridx = 3;
+        txtfTelefonos = new JTextField("",10);
+        panelDatos.add(txtfTelefonos,gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 2;
         lblNombres = new JLabel("Nombres");
         panelDatos.add(lblNombres,gbc);
 
+        gbc.gridx = 1;
+        txtfNombres = new JTextField("",10);
+        panelDatos.add(txtfNombres,gbc);
+
+        gbc.gridx = 2;
+        lblTipoContacto = new JLabel("Tipo de Contacto");
+        panelDatos.add(lblTipoContacto,gbc);
+
+        gbc.gridx = 3;
+        txtfTipoContacto = new JTextField("",10);
+        panelDatos.add(txtfTipoContacto,gbc);
+
         gbc.gridx = 0;
         gbc.gridy = 3;
         lblApellidos = new JLabel("Apellidos");
         panelDatos.add(lblApellidos,gbc);
+
+        gbc.gridx = 1;
+        txtfApellidos = new JTextField("",10);
+        panelDatos.add(txtfApellidos,gbc);
 
 
 
